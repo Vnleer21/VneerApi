8aaa035a32a6972115d80073f44c7da31c025b31
Harukai21 <allenbilly2003@gmail.com>
2024-10-14 20:52:50 +0800

    Update imagegen.js

--- api/imagegen.js
+++ api/imagegen.js
@@ -10,20 +10,20 @@ exports.config = {
 
 exports.initialize = async function ({ req, res }) {
     try {
-        // Check if there is a query parameter named 'prompt'
+        
         const imagePrompt = req.query.prompt;
         if (!imagePrompt) {
             return res.status(400).json({ error: "No image prompt provided." });
         }
 
-        // Define the model and image options
+        
         const model = 'prodia';
         const imageOptions = {
             count: 2,              // Generates 2 images by default
             nsfw: true,           // Disable NSFW content
             deepCheck: false,      // Disable deep check for NSFW content
             nsfwWords: [],         // Optional NSFW words to check the image content for
-            Api_key: 'eaca0864-70a4-4653-8dc7-f5ba3918326f', // Your API key
+            Api_key: 'eaca0864-70a4-4653-8dc7-f5ba3918326f', 
             negative_prompt: "",
             sampler: "DPM++ 2M Karras",
             height: 512,
@@ -35,11 +35,11 @@ exports.initialize = async function ({ req, res }) {
         };
 
         try {
-            // Generate the image using ApexImagine
+            
             const imageResponse = await ApexImagine(model, imagePrompt, imageOptions);
 
             if (imageResponse && imageResponse.length > 0) {
-                // Return the generated image URLs in the response
+                
                 return res.json({
                     message: `Images successfully generated for prompt: "${imagePrompt}".`,
                     images: imageResponse
