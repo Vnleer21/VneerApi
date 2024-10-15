72cef27954e868d91c3e9488c28236a6854c874d
Vnleer21 <valneer2003@gmail.com>
2024-10-15 20:31:46 +0800

    Update imagegen.js

--- api/imagegen.js
+++ api/imagegen.js
@@ -11,7 +11,7 @@ exports.config = {
     author: 'Biru',
     description: 'Generates images based on a text prompt using the Prodia model',
     category: 'art',
-    usage: ['/imagegen?prompt=pretty girl naked']
+    usage: ['/imagegen?prompt=pretty girl']
 };
 
 exports.initialize = async function ({ req, res }) {
@@ -28,7 +28,7 @@ exports.initialize = async function ({ req, res }) {
         const model = 'prodia';
         const imageOptions = {
             count: 2,              // Generates 2 images by default
-            nsfw: true,            // Disable NSFW content
+            nsfw: false,            // Disable NSFW content
             deepCheck: false,      // Disable deep check for NSFW content
             nsfwWords: [],         // Optional NSFW words to check the image content for
             Api_key: 'eaca0864-70a4-4653-8dc7-f5ba3918326f', 
